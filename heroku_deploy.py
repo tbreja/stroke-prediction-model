dff21717f5c05e4228cef66f601381252cb03313
Tubagus Moh Reja <tubagusrejaaa@gmail.com>
2021-04-10 00:32:52 +0700

    Add Existing Files

--- heroku_deploy.py
+++ heroku_deploy.py
@@ -39,12 +39,12 @@ def get_key(val, my_dict):
 
 
 image = Image.open('stroke_image.jfif')
-Context = """"Dataset Source: [Healthcare Dataset Stroke Data](https://www.kaggle.com/asaumya/healthcare-dataset-stroke-data) from Kaggle.\n
+Context = """Dataset Source: [Healthcare Dataset Stroke Data](https://www.kaggle.com/asaumya/healthcare-dataset-stroke-data) from Kaggle.\n
 This dataset is used to predict whether a patient is likely to get stroke based on the input parameters like gender, age, and various diseases and smoking status. A subset of the original train data is taken using the filtering method for Machine Learning and Data Visualization purposes.\n
 About the Data: 
 Each row in the data provides relavant information about a person , for instance; age, gender,smoking status, occurance of stroke in addition to other information
 Unknown in Smoking status means the information is unavailable. N/A in other input fields imply that it is not applicable.\n
-The Objective of this Project is to Create a model with 100 % F1 Score and AUC = 1, so the model is perfectly able to distinguish between positive class (have stroke) and negative class (not have stroke).""""
+The Objective of this Project is to Create a model with 100 % F1 Score and AUC = 1, so the model is perfectly able to distinguish between positive class (have stroke) and negative class (not have stroke)."""
 
 Link = 'https://github.com/tbreja/stroke-prediction-model.git'
 
